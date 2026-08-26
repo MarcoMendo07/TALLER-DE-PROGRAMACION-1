2626eec892e5a16e210c369e0ff609d146839e9a
Marco Mendoza Matus <marcomendozamatus07@gmail.com>
2026-08-26 12:47:31 -0300

    Add files via upload

---
+++ PRACTICA 1-4.pas
@@ -0,0 +1,35 @@
+{Una librería requiere el procesamiento de la información de sus productos. De cada
+producto se conoce el código del producto, código de rubro (del 1 al 6) y precio.
+Implementar un programa que invoque a módulos para cada uno de los siguientes
+puntos:
+
+a. Lea los datos de los productos y los almacene ordenados por código de
+producto y agrupados por rubro, en una estructura de datos adecuada. El
+ingreso de los productos finaliza cuando se lee el precio -1.
+
+b. Una vez almacenados, muestre los códigos de los productos pertenecientes a
+cada rubro.
+
+c. Genere un vector (de a lo sumo 20 elementos) con los productos del rubro 3.
+Considerar que puede haber más o menos de 20 productos del rubro 3. Si la
+cantidad de productos del rubro 3 es mayor a 20, se debe almacenar los
+primeros 20 que están en la lista e ignore el resto.
+
+d. Ordenar, por precio, los elementos del vector generado en c) utilizando el
+método visto en la teoría.
+
+e. Muestre los precios del vector resultante del punto d).
+
+f. Calcule el promedio de los precios del vector resultante del punto d). }
+
+
+program untitled;
+
+uses crt;
+var i : byte;
+
+BEGIN
+	
+	
+END.
+
